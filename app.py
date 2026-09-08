f22fe5d38863d2d54bb0809e6a0fd1a2309e4d18
JoaoFilip-a07 <joaofilipesant@gmail.com>
2026-09-08 10:21:10 -0300

    Update app.py
    Adiciona a função para cadastrar tarefas/atividades.

--- app.py
+++ app.py
@@ -1,2 +1,8 @@
 print("=== Projeto DevOps ===")
 print("Aplicação iniciada com sucesso!")
+
+tarefas = []
+
+def adicionar_tarefa(tarefa):
+    tarefas.append(tarefa)
+    print("Tarefa adicionada com sucesso!")
