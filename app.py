4632c3d1bf1fa79a417f4e14552e2ce5dede3bf5
JoaoFilip-a07 <joaofilipesant@gmail.com>
2026-09-08 10:24:49 -0300

    Adiciona uma função para remover tarefas.

--- app.py
+++ app.py
@@ -11,3 +11,10 @@ def listar_tarefas():
     print("\n=== Lista de Tarefas ===")
     for tarefa in tarefas:
         print("-", tarefa)
+
+def remover_tarefa(tarefa):
+    if tarefa in tarefas:
+        tarefas.remove(tarefa)
+        print("Tarefa removida com sucesso!")
+    else:
+        print("Tarefa não encontrada.")
